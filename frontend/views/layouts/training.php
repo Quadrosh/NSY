b49a1ea3a13315b812a46e0e7323944db6a91516
Quadrosh <quadrosh@gmail.com>
2017-02-05 14:56:03 +0300

    favicon in layouts

--- frontend/views/layouts/training.php
+++ frontend/views/layouts/training.php
@@ -45,10 +45,11 @@ IeAsset::register($this);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 
-    <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+    <link rel="shortcut icon" href="/img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="/img/favicon/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="256x256" href="/img/favicon/apple-touch-icon-256x256.png">
 
 </head>
 
