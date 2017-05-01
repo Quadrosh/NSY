07259b9c18e517fdea5df2866ed93d2500f8064b
Quadrosh <quadrosh@gmail.com>
2017-05-01 16:41:13 +0300

    1.10

--- common/widgets/menu_formfactor/svg_construct.php
+++ common/widgets/menu_formfactor/svg_construct.php
@@ -5,6 +5,7 @@
          xmlns="http://www.w3.org/2000/svg"
          xmlns:xlink="http://www.w3.org/1999/xlink"
          x="0px" y="0px"
+         width="660px" height="900px"
          viewBox="0 0 504.2 496.1"
          style="enable-background:new 0 0 504.2 496.1;"
          xml:space="preserve" >
