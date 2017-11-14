0d7cbd34bb0a48d271345b720b232098cd4f33ad
Quadrosh <quadrosh@gmail.com>
2017-11-14 19:17:36 +0300

    bot controller clean php

--- frontend/controllers/BotController.php
+++ frontend/controllers/BotController.php
@@ -8,9 +8,46 @@ use yii\helpers\Url;
 
 class BotController extends \yii\web\Controller
 {
+//    public function actionDialog()   //http://nsy.dev/475062491AAGxkvyWyk0xfbZzv5bKGZcFkaftHPTNEZQ
+//    {
+//        echo 'here';
+//    }
+
+
     public function actionDialog()   //http://nsy.dev/475062491AAGxkvyWyk0xfbZzv5bKGZcFkaftHPTNEZQ
     {
-        echo 'here';
+        define('_MY_BOT_TOKEN', '475062491:AAGxkvyWyk0xfbZzv5bKGZcFkaftHPTNEZQ');
+        define('_TELEGRAM_API_URL', 'https://api.telegram.org/bot'._MY_BOT_TOKEN.'/');
+
+// read incoming info and grab the chatID
+        $content = file_get_contents("php://input");
+        $update = json_decode($content, true);
+        $chatID = $update["message"]["chat"]["id"];
+
+// compose reply
+        $reply =  sendMessage();
+
+// send reply
+        $sendto =_TELEGRAM_API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+        file_get_contents($sendto);
+
+        function sendMessage(){
+            $message = "I am a motivator bot.";
+            return $message;
+        }
+
+// Create a debug log.txt to check the response/repy from Telegram in JSON format.
+// You can disable it by commenting checkJSON.
+        checkJSON($chatID,$update);
+        function checkJSON($chatID,$update){
+
+            $myFile = "bot_log.txt";
+            $updateArray = print_r($update,TRUE);
+            $fh = fopen($myFile, 'a') or die("can't open file");
+            fwrite($fh, $chatID ."nn");
+            fwrite($fh, $updateArray."nn");
+            fclose($fh);
+        }
     }
 
 //   https://nashe-schastye.ru/475062491AAGxkvyWyk0xfbZzv5bKGZcFkaftHPTNEZQ
