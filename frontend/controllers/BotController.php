3e3e1c18c9afd08e6e040a57bce9d39b9a02baae
Quadrosh <quadrosh@gmail.com>
2017-11-24 18:03:29 +0300

    bot work 0.05

--- frontend/controllers/BotController.php
+++ frontend/controllers/BotController.php
@@ -64,70 +64,70 @@ class BotController extends \yii\web\Controller
 
         $motivator = Motivator::find()->where(['hrurl'=>$text])->one();
 
-        if ($motivator == null) {
+//        if ($motivator == null) {
 //            Yii::$app->telegram->sendMessage([
 //                'chat_id' => $chatId,
 //                'text' => 'нет такого мотиватора',
 //            ]);
-        } else {
-            $quotes = $motivator->mLines;
-            if ($text == 'accept-the-life-daemon') {
-//                $quotesCount = count($quotes);
-//                $now = date_timestamp_get(new \DateTime());
-//                $timeMark = $now;
-//                $stepI = 1;
-//                $blockI = 1;
-//
+//        } else {
+//            $quotes = $motivator->mLines;
+//            if ($text == 'accept-the-life-daemon') {
+////                $quotesCount = count($quotes);
+////                $now = date_timestamp_get(new \DateTime());
+////                $timeMark = $now;
+////                $stepI = 1;
+////                $blockI = 1;
+////
+////                foreach ($quotes as $quote) {
+////
+////                    $process = new MotivatorBotProcess();
+////                    $process['chat_id'] = $chatId;
+////                    $process['first_name'] = $fromFirstName;
+////                    $process['chat_date'] = $date;
+////                    $process['command'] = $text;
+////                    $process['motivator_id'] = $motivator['id'];
+////                    $process['steps_qnt'] = $quotesCount;
+////
+////                    $process['current_step'] = $stepI;
+////                    $process['current_block'] = $quote['block_num'];
+////                    if ($process['current_block'] != $blockI) {
+////                        $process['new_block'] = true;
+////                        $process['start_time'] = $timeMark + 4;
+////                        $blockI++;
+////                    } else {
+////                        $process['start_time'] = $timeMark + 2;
+////                    }
+////                    $timeMark = $process['start_time'];
+////                    $process['mline_id'] = $quote['id'];
+////                    $process['text'] = $quote['text'];
+////
+////                    $process->save();
+////                    $stepI++;
+////
+////                }
+////
+////                $daemon = Daemons::find()->where(['daemon'=>'motivator-bot-daemon'])->one();
+////                if ($daemon == null) {
+////                    $daemon = new Daemons();
+////                    $daemon['daemon'] = 'motivator-bot-daemon';
+////                }
+////                $daemon['enabled'] = true;
+////                $daemon->save();
+////
+////                Yii::$app->telegram->sendMessage([
+////                    'chat_id' => $chatId,
+////                    'text' => $motivator['list_name'].' ('.$quotesCount.')',
+////                ]);
+//            } else {   // no daemon
+//                $quoteText = '';
+//                $quiteBox = null;
 //                foreach ($quotes as $quote) {
-//
-//                    $process = new MotivatorBotProcess();
-//                    $process['chat_id'] = $chatId;
-//                    $process['first_name'] = $fromFirstName;
-//                    $process['chat_date'] = $date;
-//                    $process['command'] = $text;
-//                    $process['motivator_id'] = $motivator['id'];
-//                    $process['steps_qnt'] = $quotesCount;
-//
-//                    $process['current_step'] = $stepI;
-//                    $process['current_block'] = $quote['block_num'];
-//                    if ($process['current_block'] != $blockI) {
-//                        $process['new_block'] = true;
-//                        $process['start_time'] = $timeMark + 4;
-//                        $blockI++;
-//                    } else {
-//                        $process['start_time'] = $timeMark + 2;
+//                    if ($quiteBox !=$quote['block_num']) {
+//                        $quiteBox = $quote['block_num'];
+//                        $quoteText .= '——————————————'.PHP_EOL;
 //                    }
-//                    $timeMark = $process['start_time'];
-//                    $process['mline_id'] = $quote['id'];
-//                    $process['text'] = $quote['text'];
-//
-//                    $process->save();
-//                    $stepI++;
-//
+//                    $quoteText .= $quote['text'].PHP_EOL;
 //                }
-//
-//                $daemon = Daemons::find()->where(['daemon'=>'motivator-bot-daemon'])->one();
-//                if ($daemon == null) {
-//                    $daemon = new Daemons();
-//                    $daemon['daemon'] = 'motivator-bot-daemon';
-//                }
-//                $daemon['enabled'] = true;
-//                $daemon->save();
-//
-//                Yii::$app->telegram->sendMessage([
-//                    'chat_id' => $chatId,
-//                    'text' => $motivator['list_name'].' ('.$quotesCount.')',
-//                ]);
-            } else {   // no daemon
-                $quoteText = '';
-                $quiteBox = null;
-                foreach ($quotes as $quote) {
-                    if ($quiteBox !=$quote['block_num']) {
-                        $quiteBox = $quote['block_num'];
-                        $quoteText .= '——————————————'.PHP_EOL;
-                    }
-                    $quoteText .= $quote['text'].PHP_EOL;
-                }
 //                Yii::$app->telegram->sendMessage([
 //                    'chat_id' => $chatId,
 //                    'text' => $motivator['list_name'],
@@ -137,10 +137,12 @@ class BotController extends \yii\web\Controller
 //                    'chat_id' => $chatId,
 //                    'text' => $quoteText,
 //                ]);
-            }
-        }
+//            }
+//        }
         return 'end return';
     }
+
+
 
     public function beforeAction($action)
     {
