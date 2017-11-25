ddc1ba83976f7fa9e781075fc05f5c1486a673c3
Quadrosh <quadrosh@gmail.com>
2017-11-25 14:07:49 +0300

    bot work 0.38

--- frontend/controllers/BotController.php
+++ frontend/controllers/BotController.php
@@ -198,7 +198,7 @@ class BotController extends \yii\web\Controller
 //                    $data[$i]['text'] = $motivator['list_name'];
 //                    $data[$i]['callback_data'] = $motivator['hrurl'];
                     $data[$i] = [
-                        ['text'=>$motivator['list_name'],'callback_data'=> $motivator['hrurl']],
+                        ['text'=>$motivator['list_name'],'callback_data'=> $motivator['hrurl']]
                     ];
                     $i++;
                     $rowI++;
