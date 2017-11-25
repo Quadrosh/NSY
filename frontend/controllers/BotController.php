8a4ec767c70600e69d67ba739a0121fa2d79f0d1
Quadrosh <quadrosh@gmail.com>
2017-11-25 14:24:17 +0300

    bot work 0.44

--- frontend/controllers/BotController.php
+++ frontend/controllers/BotController.php
@@ -193,10 +193,14 @@ class BotController extends \yii\web\Controller
 //                $rowI = 1;
 //                $arr = [];
                 foreach ($motivators as $motivator) {
+                    $data[$i] = ['text'=>$motivator['list_name'],'callback_data'=> $motivator['hrurl']];
+
 //                    $data[$rowI][$i] = ['text'=>$motivator['list_name'],'callback_data'=> $motivator['hrurl']];
 //                    $data[$i] = ['text'=>$motivator['list_name'],'callback_data'=> $motivator['hrurl']];
-                    $data[$i]['text'] = $motivator['list_name'];
-                    $data[$i]['callback_data'] = $motivator['hrurl'];
+
+//                    $data[$i]['text'] = $motivator['list_name'];
+//                    $data[$i]['callback_data'] = $motivator['hrurl'];
+
 //                    $data[$i] = [
 //                        ['text'=>$motivator['list_name'],'callback_data'=> $motivator['hrurl']]
 //                    ];
