44c5ee8af57a6e2d23855930cbfaa28bdfa961fa
Quadrosh <quadrosh@gmail.com>
2017-12-21 17:18:10 +0300

    play bot style 0.02

--- frontend/controllers/ChepuhaBotController.php
+++ frontend/controllers/ChepuhaBotController.php
@@ -76,7 +76,7 @@ class ChepuhaBotController extends \yii\web\Controller
                 $this->sendMessage([
                     'chat_id' => $message['chat']['id'],  // $message['from']['id']
                     'parse_mode' => 'html',
-                    'text' => 'Привет, я <b>Чепухобот</b>. Я задаю странные вопросы и составляю из твоих ответов различные предложения. Ниже список опций:',
+                    'text' => 'Привет, я <b>Чепухобот</b>. \n Я задаю странные вопросы и составляю из твоих ответов различные предложения. Ниже список опций:',
                     'reply_markup' => json_encode([
                         'inline_keyboard'=>[
                             [
