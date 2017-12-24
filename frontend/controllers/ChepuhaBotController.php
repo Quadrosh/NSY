42668cfa90d135a55b8cb2c40ed5c50845f8a8bf
Quadrosh <quadrosh@gmail.com>
2017-12-24 19:46:15 +0300

    chepuhoBot 1.03

--- frontend/controllers/ChepuhaBotController.php
+++ frontend/controllers/ChepuhaBotController.php
@@ -475,34 +475,22 @@ class ChepuhaBotController extends \yii\web\Controller
                         $this->sendMessage([
                             'chat_id' => $message['from']['id'],
                             'text' => $text,
-                            'inline_keyboard'=>[
-                                [
-                                    ['text'=>"Играть еще",'callback_data'=> 'newGame'],
-                                ],
-                                [
-                                    ['text'=>"Отправить другу",'switch_inline_query_current_chat'=> 'phrase'],
-                                ],
-                            ]
+                            'parse_mode' => 'html',
+                            'reply_markup' => json_encode([
+                                'inline_keyboard'=>[
+                                    [
+                                        ['text'=>"Играть еще",'callback_data'=> 'newGame'],
+                                    ],
+                                    [
+                                        ['text'=>"Отправить другу",'switch_inline_query_current_chat'=> 'phrase'],
+                                    ],
+                                ]
+                            ]),
+
                         ]);
                         $session->delete();
                         return 'ok';
 
-
-//                    if ($session['item_type']=='phrase') {
-//                        $play = ChBotPhrase::find()->where(['id'=>$session['item_id']])->one();
-//                        $text = $play['text'];
-//                        $vars = $session->vars;
-//                        foreach ($vars as  $var) {
-//                            $text = str_replace('#'.$var['item_var_id'],$var['value'], $text);
-//                            $var->delete();
-//                        }
-//                        $this->sendMessage([
-//                            'chat_id' => $message['from']['id'],
-//                            'text' => $text,
-//                        ]);
-//                        $session->delete();
-//                        return 'ok';
-//                    }
 
                 }
 
