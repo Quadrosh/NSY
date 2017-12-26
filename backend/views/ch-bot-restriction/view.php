425f9e4aba2f9656c42ebf9bd06b8f0c72a9d95a
Quadrosh <quadrosh@gmail.com>
2017-12-26 15:38:13 +0300

    chepuhoBot restrictions 0.02

--- backend/views/ch-bot-restriction/view.php
+++ backend/views/ch-bot-restriction/view.php
@@ -35,8 +35,24 @@ $this->params['breadcrumbs'][] = $this->title;
             'name',
             'description:ntext',
             'text:ntext',
-            'created_at',
-            'updated_at',
+            [
+                'attribute'=>'updated_at',
+                'value'=> function($data)
+                {
+                    return \Yii::$app->formatter->asDatetime($data->updated_at, 'HH:mm dd/MM/yyyy');
+
+                },
+                'format'=> 'html',
+            ],
+            [
+                'attribute'=>'created_at',
+                'value'=> function($data)
+                {
+                    return \Yii::$app->formatter->asDatetime($data->created_at, 'HH:mm dd/MM/yyyy');
+
+                },
+                'format'=> 'html',
+            ],
         ],
     ]) ?>
 
