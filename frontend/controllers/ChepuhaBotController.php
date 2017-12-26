218741a3d585d3938b0672b498fdd12e930933d5
Quadrosh <quadrosh@gmail.com>
2017-12-26 17:32:38 +0300

    chepuhoBot permission 0.11

--- frontend/controllers/ChepuhaBotController.php
+++ frontend/controllers/ChepuhaBotController.php
@@ -376,7 +376,7 @@ class ChepuhaBotController extends \yii\web\Controller
                                 'inline_keyboard'=>[
                                     [
 //                                        ['text'=>"Продолжить", 'callback_data'=> 'addPermission/'.$restriction['short'].'/'.$message['text']],
-                                        ['text'=>"Продолжить", 'callback_data'=> 'addPermission/'.$restriction['short'].'/'.$message['text']],
+                                        ['text'=>"Продолжить", 'callback_data'=> 'addPermission/'.'/'],
                                     ],
                                     [
                                         ['text'=>"Вернуться", 'callback_data'=> 'newGame'],
