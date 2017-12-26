f601aaf5353f40763cf8402b2ee224383b12435b
Quadrosh <quadrosh@gmail.com>
2017-12-26 22:11:57 +0300

    chepuhoBot permission 0.31

--- frontend/controllers/ChepuhaBotController.php
+++ frontend/controllers/ChepuhaBotController.php
@@ -191,7 +191,7 @@ class ChepuhaBotController extends \yii\web\Controller
                     'chat_id' => $message['chat']['id'],  // $message['from']['id']
                     'parse_mode' => 'html',
 //                    'text' => $message['from']['username'],
-                    'text' => Html::encode('дальше U+27A1'.hex2bin('\xE2\x9E\xA1').hex2bin('f09f9880').'\xE2\x9E\xA1'),
+                    'text' => Html::encode('дальше U+27A1'.hex2bin('\xE2\x9E\xA1').hex2bin('f09f9880').'\xE2\x9E\xA1').'\xE2\x9E\xA1',
 //                    'text' => json_encode($user->save()),
 //                    'text' => json_encode($user->hasErrors()),
 
