0af1c03d9589a546917d87cde66475675660f0ae
Quadrosh <quadrosh@gmail.com>
2017-12-26 18:40:48 +0300

    chepuhoBot permission 0.20

--- frontend/controllers/ChepuhaBotController.php
+++ frontend/controllers/ChepuhaBotController.php
@@ -334,112 +334,113 @@ class ChepuhaBotController extends \yii\web\Controller
             elseif (substr($message['text'],0,5) == 'play/' OR  substr($message['text'],0,7) == 'phrase/'){
 
 
+                $this->newGame($message);
 
-                $commands = explode('/', $message['text']);
-                $type = $commands[0];
-                $hrurl = $commands[1];
-
-                if ($type == 'play') {
-                    $play = ChBotPlay::find()->where(['hrurl'=>$hrurl])->one();
-                } elseif ($type == 'phrase') {
-                    $play = ChBotPhrase::find()->where(['hrurl'=>$hrurl])->one();
-                } else {
-                    $play = ChBotPlay::find()->where(['hrurl'=>$hrurl])->one();
-                }
-
-
-
-                $user = BotUser::find()->where(['user_id'=>$message['from']['id']])->one();
-
-                if ($user == null) {
-                    $user = new BotUser();
-                    $user['user_id'] = $message['from']['id'];
-                    $user['first_name'] = $message['from']['first_name'];
-                    $user['last_name'] = $message['from']['last_name'];
-                    $user['username'] = $message['from']['username'];
-                    $user['language_code'] = $message['from']['language_code'];
-                    $user->save();
-                }
-
-//              18+
-                if ($play->restriction) {
-
-                    $restriction = $play->restriction;
-
-                    if (!$user->hasPermission($restriction['short'])) {
-
-                        $this->sendMessage([
-                            'chat_id' => $message['from']['id'],
-                            'text' => $restriction['text'],
-                            'reply_markup' => json_encode([
-                                'inline_keyboard'=>[
-                                    [
-                                        ['text'=>"Вернуться", 'callback_data'=> 'newGame'],
-                                        ['text'=>"Продолжить", 'callback_data'=> 'addPermission/'.$restriction['short'].'/'.$type.'/'.$play['id']],
-                                    ],
-
-                                ]
-                            ]),
-                        ]);
-                        return [
-                            'message' => 'ok',
-                            'code' => 200,
-                        ];
-                    }
-                }
-
-                $use = new BotUse();
-                $use['bot_name'] = 'ChepuhoBot';
-                $use['user_id'] = $user['id'];
-                $use['item_type'] = $type;
-                $use['item_id'] = $play['id'];
-                $use['done'] = 'start';
-                $use->save();
-
-                $session = ChBotSession::find()->where(['user_id'=>$message['from']['id']])->one();
-
-                if ($session == null) {
-                    $session = new ChBotSession;
-                    $session['user_id'] = $message['from']['id'];
-                } else {
-                    $sessionVars = $session->vars;
-                    if ($sessionVars != null) {
-                        foreach ($sessionVars as $sessionVar) {
-                            $sessionVar->delete();
-                        }
-                    }
-                }
-
-                $session['item_type'] = $type;
-                $session['item_id'] = $play['id'];
-                $session['description'] = strval($use['id']);
-                $session->save();
-
-                $playVars = $play->vars;
-                $sessionVars = $session->vars;
-                if ($sessionVars == null) {
-                    foreach ($playVars as $playVar) {
-                        $sessionVar = new ChBotSessionVars();
-                        $sessionVar['session_id'] = $session['id'];
-                        $sessionVar['item_var_id'] = $playVar['id'];
-                        $sessionVar['question'] = $playVar['question'];
-                        $sessionVar['status'] = 'raw';
-                        $sessionVar->save();
-                    }
-                }
-                $goQuestion = ChBotSessionVars::find()->where(['session_id'=>$session['id'],'status'=>'raw'])->one();
-                $goQuestion['status'] = 'active';
-                $goQuestion->save();
-
-                $this->sendMessage([
-                    'chat_id' => $message['from']['id'],
-                    'text' => $goQuestion['question'],
-                ]);
-
-                return [
-                    'message' => 'ok',
-                    'code' => 200,
-                ];
+//                $commands = explode('/', $message['text']);
+//                $type = $commands[0];
+//                $hrurl = $commands[1];
+//
+//                if ($type == 'play') {
+//                    $play = ChBotPlay::find()->where(['hrurl'=>$hrurl])->one();
+//                } elseif ($type == 'phrase') {
+//                    $play = ChBotPhrase::find()->where(['hrurl'=>$hrurl])->one();
+//                } else {
+//                    $play = ChBotPlay::find()->where(['hrurl'=>$hrurl])->one();
+//                }
+//
+//
+//
+//                $user = BotUser::find()->where(['user_id'=>$message['from']['id']])->one();
+//
+//                if ($user == null) {
+//                    $user = new BotUser();
+//                    $user['user_id'] = $message['from']['id'];
+//                    $user['first_name'] = $message['from']['first_name'];
+//                    $user['last_name'] = $message['from']['last_name'];
+//                    $user['username'] = $message['from']['username'];
+//                    $user['language_code'] = $message['from']['language_code'];
+//                    $user->save();
+//                }
+//
+////              18+
+//                if ($play->restriction) {
+//
+//                    $restriction = $play->restriction;
+//
+//                    if (!$user->hasPermission($restriction['short'])) {
+//
+//                        $this->sendMessage([
+//                            'chat_id' => $message['from']['id'],
+//                            'text' => $restriction['text'],
+//                            'reply_markup' => json_encode([
+//                                'inline_keyboard'=>[
+//                                    [
+//                                        ['text'=>"Вернуться", 'callback_data'=> 'newGame'],
+//                                        ['text'=>"Продолжить", 'callback_data'=> 'addPermission/'.$restriction['short'].'/'.$type.'/'.$play['id']],
+//                                    ],
+//
+//                                ]
+//                            ]),
+//                        ]);
+//                        return [
+//                            'message' => 'ok',
+//                            'code' => 200,
+//                        ];
+//                    }
+//                }
+//
+//                $use = new BotUse();
+//                $use['bot_name'] = 'ChepuhoBot';
+//                $use['user_id'] = $user['id'];
+//                $use['item_type'] = $type;
+//                $use['item_id'] = $play['id'];
+//                $use['done'] = 'start';
+//                $use->save();
+//
+//                $session = ChBotSession::find()->where(['user_id'=>$message['from']['id']])->one();
+//
+//                if ($session == null) {
+//                    $session = new ChBotSession;
+//                    $session['user_id'] = $message['from']['id'];
+//                } else {
+//                    $sessionVars = $session->vars;
+//                    if ($sessionVars != null) {
+//                        foreach ($sessionVars as $sessionVar) {
+//                            $sessionVar->delete();
+//                        }
+//                    }
+//                }
+//
+//                $session['item_type'] = $type;
+//                $session['item_id'] = $play['id'];
+//                $session['description'] = strval($use['id']);
+//                $session->save();
+//
+//                $playVars = $play->vars;
+//                $sessionVars = $session->vars;
+//                if ($sessionVars == null) {
+//                    foreach ($playVars as $playVar) {
+//                        $sessionVar = new ChBotSessionVars();
+//                        $sessionVar['session_id'] = $session['id'];
+//                        $sessionVar['item_var_id'] = $playVar['id'];
+//                        $sessionVar['question'] = $playVar['question'];
+//                        $sessionVar['status'] = 'raw';
+//                        $sessionVar->save();
+//                    }
+//                }
+//                $goQuestion = ChBotSessionVars::find()->where(['session_id'=>$session['id'],'status'=>'raw'])->one();
+//                $goQuestion['status'] = 'active';
+//                $goQuestion->save();
+//
+//                $this->sendMessage([
+//                    'chat_id' => $message['from']['id'],
+//                    'text' => $goQuestion['question'],
+//                ]);
+//
+//                return [
+//                    'message' => 'ok',
+//                    'code' => 200,
+//                ];
             }
 
 
