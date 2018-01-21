739f93737b5ef1839ffc712c602fd8ce11a68246
Quadrosh <quadrosh@gmail.com>
2018-01-21 04:11:41 +0300

    bot use stat2

--- backend/views/bot-use/stat.php
+++ backend/views/bot-use/stat.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Bot Games Stat';
+$this->title = 'Bot Games';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="bot-use-index">
