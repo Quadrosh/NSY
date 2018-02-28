273d82573f433a5ae887295f0345196d9e34e9bb
Quadrosh <quadrosh@gmail.com>
2018-02-28 18:11:01 +0300

    b2b server gzip 0.1

--- frontend/controllers/B2bBotController.php
+++ frontend/controllers/B2bBotController.php
@@ -668,7 +668,8 @@ class B2bBotController extends \yii\web\Controller
         $optQuery = http_build_query($options);
         $ch = curl_init($url.'?'.$optQuery);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch,CURLOPT_ENCODING , "gzip"); // gzip
+//        curl_setopt($ch,CURLOPT_ENCODING , "gzip"); // gzip
+        curl_setopt($ch,CURLOPT_ENCODING , ''); // all
         curl_setopt($ch, CURLOPT_USERAGENT, 'Bot');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         if (count($options)) {
